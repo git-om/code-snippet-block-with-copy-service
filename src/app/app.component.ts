f925c4576d280e67b7f774f4e4d1faba2f67136b
git-om <chaithanya.omsai@gmail.com>
2020-01-20 15:05:57 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component, ViewChildren, ElementRef, AfterViewInit, QueryList, ViewEnca
 })
 export class AppComponent implements AfterViewInit {
   title = 'Copy Codesnippet';
-  name = 'Automating Code Snippets';
   public buttonList: ElementRef[] = [];
 
   @ViewChildren('buttonsRefs') buttons: QueryList<ElementRef>;
